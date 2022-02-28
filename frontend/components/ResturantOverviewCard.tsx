b2aa2eb6650364da160edb19300c40b89542ad89
abassey <anemmeabasi1@gmail.com>
2022-02-28 17:13:01 -0500

    progress bar added for card swiping, package patch added for useNativeDriver warning, still not fixed

--- frontend/components/ResturantOverviewCard.tsx
+++ frontend/components/ResturantOverviewCard.tsx
@@ -1,17 +1,38 @@
 import { React, Component } from 'react';
-import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { View, Text, Pressable, StyleSheet, Dimensions } from 'react-native';
 import Swiper from 'react-native-deck-swiper';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-
+import ProgressBarAnimated from 'react-native-progress-bar-animated';
+import ProgressBar from 'react-native-animated-progress';
 
 class Session extends Component {
     data = ['this', 'is', 'a', 'resturant', 'overview', 'card', 'almost', 'at', 'the', 'end'];
-    
+    //{console.log('card at index ' + cardIndex +' swiped')}
+    state = {
+        progress: 0,
+    }
+
+    increment() {
+        this.setState((state) => {
+            return {progress: state.progress + this.data.length}
+        }), () => 
+        console.log(this.state.progress);
+    }
+
     render () {
+        const barWidth = Dimensions.get('screen').width - 50;
+
         return (
             <View style={styles.container}>
-                <Text>This is where the timer and progree bar will go</Text>
-                <Swiper
+                <Text>This is where the timer will go</Text>
+                <View style={styles.progressBar}>
+                    <ProgressBarAnimated
+                        width={barWidth}
+                        value={this.state.progress}
+                        backgroundColorOnComplete="#6CC644"
+                    />
+                </View>
+                    <Swiper
                     ref={swiper => {
                         this.swiper = swiper;
                     }}
@@ -23,7 +44,7 @@ class Session extends Component {
                             </View>
                         )
                     }}
-                    onSwiped={(cardIndex) => {console.log(cardIndex)}}
+                    onSwiped={() =>  {this.increment()}}
                     onSwipedAll={() => {console.log('onSwipedAll')}}
                     onSwipedLeft={(cardIndex) => {console.log('card at index ' + cardIndex +' swiped no')}}
                     onSwipedRight={(cardIndex) => {console.log('card at index ' + cardIndex +' swiped like')}}
@@ -34,7 +55,7 @@ class Session extends Component {
                     backgroundColor={'#ffffff'}
                     stackSize= {3}
                     marginBottom={100}
-                    marginTop={40}>
+                    marginTop={100}>
                 </Swiper>
                 <View style={styles.buttonRow}>
                     <Pressable onPress={() => this.swiper.swipeLeft()}>
@@ -63,10 +84,10 @@ const styles = StyleSheet.create({
       backgroundColor: "#ffffff",
     },
     card: {
-      flex: 0.75,
+      flex: 0.8,
       borderRadius: 5,
       padding: 10,
-      borderWidth: 2,
+      borderWidth: 3,
       borderColor: "#000000",
       justifyContent: "center",
       backgroundColor: "#E8E8E8",
@@ -79,8 +100,13 @@ const styles = StyleSheet.create({
     buttonRow: {
         justifyContent: "space-between",
         flexDirection: "row",
-        marginTop: 650,
+        marginTop: 600,
         paddingRight: 30,
         paddingLeft: 30,
+    },
+    progressBar: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 60
     },
   });
