9cf2218250800b97a9e3bad6223fc62caa297230
student <student@w22-4250-01.socs.uoguelph.ca>
2022-03-11 14:19:39 -0500

    added indices

--- api/app.py
+++ api/app.py
@@ -87,7 +87,7 @@ def getResturantInfo():
 
     restaurants = select(restaurant for restaurant in Restaurant if restaurant.PriceHigh >= float(request.args.get('price-high')) \
         and restaurant.PriceLow <= float(request.args.get('price-low')) and restaurant.Rating >= float(request.args.get('rating')) \
-        and request.args.get('cuisine') in restaurant.CuisineType)[:]
+        and request.args.get('cuisine') in restaurant.CuisineType)[request.args.get('start-index'):request.args.get('end-index')]
 
     for resturant in restaurants:
         to_return.append({"name": resturant.Name, "location": resturant.Location, "hours": resturant.HoursOfOperation,
