ae341b1e80d91b04dcfa11b912ee3c1d907d6ed1
abassey <anemmeabasi1@gmail.com>
2022-03-16 23:01:53 -0400

    fixed worng merge destination branch

--- api/app.py
+++ api/app.py
@@ -121,20 +121,22 @@ def create_group():
 
 ### Restaurants ###
 
-@app.route('/resturant/query', methods=["GET"])
-def getResturantInfo():
+@app.route('/restaurant/query', methods=["GET"])
+def getRestaurantInfo():
     to_return = []
 
     restaurants = select(restaurant for restaurant in Restaurant if restaurant.PriceHigh >= float(request.args.get('price-high')) \
         and restaurant.PriceLow <= float(request.args.get('price-low')) and restaurant.Rating >= float(request.args.get('rating')) \
         and request.args.get('cuisine') in restaurant.CuisineType)[int(request.args.get('start-index')):int(request.args.get('end-index'))]
 
-    for resturant in restaurants:
-        to_return.append({"name": resturant.Name, "location": resturant.Location, "hours": resturant.HoursOfOperation,
-         "website": resturant.Website, "phone": resturant.PhoneNumber, "dining-option": resturant.DiningType, "bookingsite": resturant.BookingSite,
-         "picture": resturant.PictureLocation, "sponsored": resturant.Sponsored, "cuisine": resturant.CuisineType, "rating": resturant.Rating, 
-         "price-low": resturant.PriceLow, "price-high": resturant.PriceHigh,})
-    return {"resturants": to_return}
+    for restaurant in restaurants:
+        i = 0
+        to_return.append({"id": i, "name": restaurant.Name, "location": restaurant.Location, "hours": restaurant.HoursOfOperation,
+         "website": restaurant.Website, "phone": restaurant.PhoneNumber, "dining_option": restaurant.DiningType, "bookingsite": restaurant.BookingSite,
+         "picture": restaurant.PictureLocation, "sponsored": restaurant.Sponsored, "cuisine": restaurant.CuisineType, "rating": restaurant.Rating, 
+         "price_low": restaurant.PriceLow, "price_high": restaurant.PriceHigh,})
+        i = i + 1
+    return {"restaurants": to_return}
 
 ### End Restaurants ###
 
