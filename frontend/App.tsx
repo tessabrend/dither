b9dc79f7964b2f9f7fa793187326341c240bdd65
Tessa Brenders <tbrender@uoguelph.ca>
2022-03-17 03:31:05 -0400

    fetch added

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,7 +2,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Navigation from './navigation';
 import { StatusBar } from 'expo-status-bar';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHeart, faCircleXmark, faFaceGrinStars, faFaceFrown, faDollarSign, faCircleUser } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faCircleXmark, faFaceGrinStars, faFaceFrown, faDollarSign, faCircleUser, faAngleRight, faCrown } from '@fortawesome/free-solid-svg-icons';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import React, { useEffect } from 'react';
@@ -12,7 +12,7 @@ export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
-  library.add(faHeart, faCircleXmark, faFaceGrinStars, faFaceFrown, faDollarSign, faCircleUser );
+  library.add(faHeart, faCircleXmark, faFaceGrinStars, faFaceFrown, faDollarSign, faCircleUser, faAngleRight, faCrown );
 
   const checkUserExists = async () => {
   }
