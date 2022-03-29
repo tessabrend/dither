f995e0d2f138e0d16636bc63f4bb7052076f5231
abassey <anemmeabasi1@gmail.com>
2022-03-29 18:04:54 -0400

    added external link redirects

--- frontend/components/AllDetails.tsx
+++ frontend/components/AllDetails.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import { StyleSheet, Image, TouchableOpacity } from "react-native";
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import Colors from '../constants/Colors';
+import React, {Component} from "react";
+import { StyleSheet, Image, Pressable, TouchableOpacity } from "react-native";
 import { Text, View } from './Themed';
-import CardFlip from 'react-native-card-flip';
+import FlipCard from "react-native-flip-card-plus";
+import * as WebBrowser from 'expo-web-browser';
+import * as Linking from 'expo-linking';
+import { Feather } from '@expo/vector-icons';
 
 const enum Price {
   Low = '$',
@@ -11,124 +12,145 @@ const enum Price {
   High = '$$$'
 }
 
-export default function ConsensusMade() {
-  const restaurantData = ['Restaurant Name' , Price.Medium, '0.5km', 'Pub' ];//temp data
+export default class AllDetailsCard extends Component {
+  restaurantData = ['Restaurant Name' , Price.Medium, '0.5km', 'Pub', 
+  'https://google.com', '000-000-0000', '123 Alphabet Street', '3 stars', 'https://www.opentable.ca/r/' ];//temp data
 
-  // <View style={styles.background}>
-  //     <View style={styles.headerWrapper}>
-  //       <Text style={styles.title}>Consensus Made!</Text>
-  //     </View>
+  openWebsite = () => {
+    WebBrowser.openBrowserAsync(this.restaurantData[4]);
+  };
 
-  //     <View style={styles.container}>
-  //       <Text style={styles.name}>{restaurantData[0]}</Text>
-  //       <View style={styles.detailRow}>
-  //         <View style={styles.hiddenCard}>
-  //           <FontAwesomeIcon icon="dollar-sign" size={20}/>
-  //           <FontAwesomeIcon icon="dollar-sign" size={20}/>
-  //         </View>
-  //         <View style={styles.hiddenCard}>
-  //           <Text style={styles.detailText}>{restaurantData[2]}</Text>
-  //         </View>
-  //         <View style={styles.cuisineCard}>
-  //           <Text style={styles.detailText}>{restaurantData[3]}</Text>
-  //         </View>
-  //       </View>
+  makeReservation = () => {
+    WebBrowser.openBrowserAsync(this.restaurantData[8]);
+  };
 
-  //       {/* replace with mapview when provider added */}
-  //       <Image style={styles.mapbox}
-  //             source={{uri: 'https://i.stack.imgur.com/SlwjS.png'}}
-  //           />
-  //     </View>
+  makeCall = () => {
+    Linking.openURL(`tel:${this.restaurantData[5]}`);
+  };
 
-  //     <View style={styles.footer}></View>
-  //   </View>
+  viewInMap = () => {
+    let url = 'https://www.google.com/maps/search/?api=1&query='+this.restaurantData[6]
+    Linking.openURL(url)
+  };
 
-
-  return (
-    <View>
-        <CardFlip style={styles.container} ref={(card) => this.card = card} >
-    <TouchableOpacity style={styles.background} onPress={() => this.card.flip()} ><Text>AB</Text></TouchableOpacity>
-    <TouchableOpacity style={styles.background} onPress={() => this.card.flip()} ><Text>CD</Text></TouchableOpacity>
-  </CardFlip>
-    </View>
-  );
+  render () {
+    return (
+      <View style={styles.container}>
+        <FlipCard
+          flipDirection={"h"}
+          style={styles.cardContainer}
+          ref={(card) => (this.card = card)}>
+          <Pressable
+            style={styles.card}
+            onPress={() =>this.card.flipVertical()}>
+            <Text style={styles.label}>{this.restaurantData[0]}</Text>
+            <View style={styles.infoRow}>
+            <Text style={styles.rowMember}>{this.restaurantData[1]}</Text>
+              <Text style={styles.rowMember}>{this.restaurantData[2]}</Text>
+              <Text style={styles.rowMember}>{this.restaurantData[3]}</Text>
+            </View>
+            <Image style={styles.mapbox}
+               source={{uri: 'https://i.stack.imgur.com/SlwjS.png'}}
+             />
+          </Pressable>
+          <Pressable style={styles.card} onPress={() => this.card.flipVertical()}>
+            <TouchableOpacity onPress={this.makeCall}>
+            <Text style={styles.rowMember}>PHONE: {this.restaurantData[5]}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.viewInMap}>
+            <Text style={styles.rowMember}>LOCATION: {this.restaurantData[6]}</Text>
+            </TouchableOpacity>
+            <Text style={styles.rowMember}>RATING: {this.restaurantData[7]}</Text>
+            <View style={styles.buttonCol}>
+              <View style={styles.buttons}>
+                <Feather.Button name="arrow-up-right" size={24} backgroundColor="#000000" color="white"onPress={this.openWebsite}>
+                Website
+              </Feather.Button>
+              </View>
+              <View style={styles.buttons}>
+                <Feather.Button name="arrow-up-right" size={24} backgroundColor="#000000" color="white"onPress={this.makeReservation}>
+                Reserve
+              </Feather.Button>
+              </View>
+            </View>
+          </Pressable>
+        </FlipCard>
+        <View style={styles.manualTriggers}>
+          <Pressable
+            style={styles.trigger}
+            onPress={() => this.card.flipVertical()}>
+            <Text style={{ color: 'white' }}>Expand Details</Text>
+          </Pressable>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  background: {
-    backgroundColor: Colors.light.background,
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  headerWrapper: {
-    width: "100%",
-    height: "15%",
-    maxHeight: "15%",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "transparent",
-  },
-  header: {
-    fontSize: 30,
-    fontWeight: 'bold',
-  }, 
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    justifyContent: "center",
-    alignSelf: "center"
-  }, 
-  name: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    justifyContent: 'flex-start',
-    alignSelf: "center",
-    paddingBottom: "3%"
-  },
   container: {
     flex: 1,
-    backgroundColor: Colors.light.background,
-    borderRadius: 25,
-    borderWidth: 1,
-    borderColor: Colors.dark.background,
-    borderStyle: "solid",
-    justifyContent: "center",
-    flexDirection: 'column',
-    width: "75%",
-    height: "70%",
-    margin: "2%"
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
-  hiddenCard: {
-    backgroundColor: Colors.light.background,
-    justifyContent: "center",
-    flexDirection: "row",
-    padding: "1%",
+  cardContainer: {
+    width: 300,
+    height: 400,
   },
-  cuisineCard: {
-    borderColor: Colors.dark.background,
-    borderRadius: 10,
-    borderStyle: "solid",
-    borderWidth: 1,
-    backgroundColor: Colors.light.background,
-    justifyContent: "center",
-    flexDirection: "row",
-    padding: "1%",
+  card: {
+    width: 300,
+    height: 400,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#D8CFCD',
+    borderRadius: 5,
+    borderColor: '#000000',
+    shadowColor: 'rgba(0,0,0,0.5)',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.5,
   },
-  subtitleText: {
-    fontSize: 22,
-    fontStyle: 'italic',
-    textAlign: 'center'
+  label: {
+    textAlign: 'center',
+    fontSize: 30,
+    fontFamily: 'System',
+    color: '#000000',
+    backgroundColor: 'transparent',
   },
-  detailRow: {
-    justifyContent: "space-evenly",
-    flexDirection: "row",
-    alignItems: "center",
+  manualTriggers: {
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    justifyContent: 'space-between',
   },
-  detailText: {
-    textAlign: "center",
+  trigger: {
+    backgroundColor: 'black',
+    margin: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+    shadowColor: 'rgba(0,0,0,0.5)',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.5,
+  },
+  infoRow: {
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+  },
+  rowMember: {
+    textAlign: 'center',
     fontSize: 20,
-    justifyContent: "space-around"
+    fontFamily: 'System',
+    color: '#000000',
+    backgroundColor: 'transparent',
+    margin: 15,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
   },
   mapbox:{
     width: '75%',
@@ -136,10 +158,15 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     margin: '4%'
   },
-  footer: {
-    margin: "2%",
-    flexDirection: 'column-reverse',
-    alignContent: 'flex-end'
+  buttonCol: {
+    flexDirection: 'column',
+    color: '#ffffff',
+    backgroundColor: 'transparent',
   },
+  buttons: {
+    marginVertical: 10,
+    backgroundColor: 'transparent',
+  }
+
 });
 
